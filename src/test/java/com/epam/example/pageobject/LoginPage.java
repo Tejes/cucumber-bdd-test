ea81166cdb1afdf0840de7d3461fe12ba789335d
Epam <epamuser.test@gmail.com>
2018-04-13 16:44:47 +0200

    Add cucumber skeleton and test cases

---
+++ src/test/java/com/epam/example/pageobject/LoginPage.java
@@ -0,0 +1,21 @@
+package com.epam.example.pageobject;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+/**
+ * Page object for Login page.
+ */
+public class LoginPage {
+    @FindBy(id = "email")
+    public WebElement emailField;
+
+    @FindBy(id = "passwd")
+    public WebElement passwordField;
+
+    @FindBy(id = "SubmitLogin")
+    public WebElement submitButton;
+
+    @FindBy(css = ".alert > ol > li")
+    public WebElement errorMessage;
+}
